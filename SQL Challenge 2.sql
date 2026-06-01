4581a8a86ac31b7cf3ab41f48fb6dea09346f1d5
Olanrewaju <133510424+Wajuola177@users.noreply.github.com>
2026-06-01 22:43:34 +0100

    Add files via upload

---
+++ SQL Challenge 2.sql
@@ -0,0 +1,34 @@
+/* 1.A marketing campaign needs to target accounts 
+assigned to sales representatives "jillian0" or "pamela0". Trace all matches.*/
+
+SELECT SalesPerson
+FROM salesLT.Customer
+WHERE SalesPerson IN ('jillian0', 'pamela0');
+
+--No matching accounts found 
+-- there are no records assigned to sales representatives 'jillian0' or 'pamela0' in the system.
+
+
+/* 2.Complete an auditing verification on transactions processed above $10,000 in total dues.*/
+
+SELECT TotalDue
+FROM[SalesLT].[SalesOrderHeader]
+WHERE TotalDue > 10000;
+
+/* 3. List the Products that do not have a parent product category ID. Display their
+product category ID and name only. (hint, use "IS NULL" keyword in your query) */
+
+SELECT ProductCategoryID,ParentProductCategoryID, Name
+FROM [SalesLT].[ProductCategory]
+WHERE ParentProductCategoryID IS NULL;
+
+/* 4. Retrieve a detailed list of our products whose color is black. 
+Provide the Product name, color alongside its associated list price. 
+Display only the top 11 most expensive items. */
+
+SELECT TOP 11 Name,Color, ListPrice
+FROM [SalesLT].[Product]
+WHERE Color = 'Black'
+ORDER BY ListPrice DESC;
+
+
